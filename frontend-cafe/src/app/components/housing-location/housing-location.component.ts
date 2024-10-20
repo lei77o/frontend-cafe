d0a19e016f3a61c52919ea9237e28fdc8b018477
lalbrecht <lalbrecht@fxone.com>
2024-10-20 11:38:57 -0300

    implemented mapboxmap

--- frontend-cafe/src/app/components/housing-location/housing-location.component.ts
+++ frontend-cafe/src/app/components/housing-location/housing-location.component.ts
@@ -1,19 +1,34 @@
-import { Component, Input } from '@angular/core';
-import { HousingLocation } from '../../models/housinglocation';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MapsComponent } from '../../components/maps/maps.component';
+import { HousingLocation } from '../../models/housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CommonModule, MatGridListModule, MatIconModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CommonModule,
+    MatGridListModule,
+    MatIconModule,
+    MapsComponent,
+  ],
   templateUrl: './housing-location.component.html',
-  styleUrl: './housing-location.component.css'
+  styleUrl: './housing-location.component.css',
 })
 export class HousingLocationComponent {
-  @Input() housingLocation!: HousingLocation [];
+  @Input() housingLocation!: HousingLocation[];
+  readonly dialog = inject(MatDialog);
+
+  openLocation() {
+    const dialogRef = this.dialog.open(MapsComponent, {
+      data: { latitud: '', lng: '' },
+    });
+  }
 }
--- frontend-cafe/src/app/components/maps/maps.component.ts
+++ frontend-cafe/src/app/components/maps/maps.component.ts
@@ -1,28 +1,82 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatSliderModule } from '@angular/material/slider';
 
-import * as mapboxgl from 'mapbox-gl';
+import { LngLat, Map } from 'mapbox-gl';
 
-(mapboxgl as any).accesToken = '';
+//(mapboxgl as any).accesToken = '';
 
 @Component({
   selector: 'app-maps',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSliderModule,
+    CommonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+  ],
   templateUrl: './maps.component.html',
   styleUrl: './maps.component.css',
 })
 export class MapsComponent implements AfterViewInit {
   @ViewChild('map') public divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(60, 31);
+
   ngAfterViewInit(): void {
     if (!this.divMap) throw 'Element didnt find';
 
-    const map = new mapboxgl.Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v9',
       projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
-      zoom: 1,
-      center: [30, 15],
+      zoom: this.zoom,
+      center: this.currentLngLat,
     });
+  }
+
+  mapListeners() {
+    if (!this.map) throw 'Have no map';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+      const { lng, lat } = this.currentLngLat;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 }
